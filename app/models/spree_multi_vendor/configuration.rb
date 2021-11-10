c4351b5afdfb1974c38ed8a29cb0e3af28c24c7f
Truong Vo <vhtruongvn@gmail.com>
2021-11-10 20:45:28 +0700

    remove shipping_method from DEFAULT_VENDORIZED_MODELS

--- app/models/spree_multi_vendor/configuration.rb
+++ app/models/spree_multi_vendor/configuration.rb
@@ -1,6 +1,6 @@
 module SpreeMultiVendor
   class Configuration < ::Spree::Preferences::Configuration
-    DEFAULT_VENDORIZED_MODELS ||= %w[product variant stock_location shipping_method].freeze
+    DEFAULT_VENDORIZED_MODELS ||= %w[product variant stock_location].freeze
 
    # Some example preferences are shown below, for more information visit:
    # https://guides.spreecommerce.org/developer/core/preferences.html
